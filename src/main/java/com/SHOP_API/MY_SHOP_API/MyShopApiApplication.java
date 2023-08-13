1fa895cbad77777fd089b4f6e32bdc0ce3d08468
pupun502 <0729plp@gmai.com>
2023-08-13 18:12:08 +0530

    basics are ready

---
+++ src/main/java/com/SHOP_API/MY_SHOP_API/MyShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.SHOP_API.MY_SHOP_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyShopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SHOP_API/MY_SHOP_API/MyShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SHOP_API.MY_SHOP_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
